cefa9b923c50c4cac7714273f04f8da43bd0da12
Pawan Saket <pawan@vujadelabs.com>
2019-01-27 16:46:11 +0530

    removing app.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import './css/App.css';
-import Dashboard from './components/dashboard/Dashboard';
+import React, { Component } from "react";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Dashboard />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
